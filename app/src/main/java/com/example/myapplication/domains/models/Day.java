ec2fc76cbe5945642a34165f51663bbc012aaa1b
gschhh <alexshashin.2023@gmail.com>
2026-05-23 17:58:37 +0500

    Разработанные классы Model и ViewModel. Шашин

---
+++ app/src/main/java/com/example/myapplication/domains/models/Day.java
@@ -0,0 +1,17 @@
+package com.example.myapplication.domains.models;
+
+import java.util.stream.Stream;
+
+public class Day {
+    public String Name;
+    public Integer Temp;
+    public String Condition;
+    public Day(String name, int temp, String condition) {
+        this.Name = name;
+        this.Temp = temp;
+        this.Condition = condition;
+    }
+    public String getTemp() {
+        return this.Temp + "°";
+    }
+}
